57bb10f611ccdc5c59976e16dfcfbb78cf080e69
Syed Muhammad Kamran <smkamran2012@gmail.com>
2024-04-03 04:55:50 +0500

    todos list by using while loop

---
+++ main.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let todoQuestions = await inquirer.prompt([
+        {
+            name: "firstquestion",
+            type: "input",
+            message: "What Would you like to add in your todos?",
+        },
+        {
+            name: 'secondQuestion',
+            type: "confirm",
+            message: "Would you like to add more in your todos?",
+            defult: "true"
+        }
+    ]);
+    todos.push(todoQuestions.firstquestion);
+    console.log(todos);
+    condition = todoQuestions.secondQuestion;
+}
